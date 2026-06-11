8d0f24889e23bac2ed12ab9b11b023d5b287c610
ak-kumawat <ajaykumarkumawat608@gmail.com>
2026-06-11 17:46:36 +0530

    add middleware and utility functions for file uploads and error handling

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,7 +4,16 @@ dotenv.config({
 })
 import connectDb from "./db/index.js";
 connectDb()
-
+.then(()=>{
+    app.on((error),()=>{
+                console.log("error :",error)
+                throw error
+            })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT} `);
+        
+    })
+})
 .catch((err)=>{
     console.log("MONGODB CONNECTION FAILURE !!",err);
     
